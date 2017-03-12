6681947993298b0e33e7f65f5a6c96d786110fa4
Rene Windegger <rene@windegger.wtf>
2017-03-12 15:47:28 +0100

    Created the expected behavior.

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -32,12 +32,16 @@ namespace Server
                     Console.WriteLine("Reading message.");
                     using (NetworkStream stream = client.GetStream())
                     {
+                        StringBuilder sb = new StringBuilder();
                         byte[] buffer = new byte[32];
-                        int len = await stream.ReadAsync(buffer, 0, buffer.Length);
-                        string incomingMessage = Encoding.UTF8.GetString(buffer, 0, len);
-                        Console.WriteLine("Incoming message: {0}", incomingMessage);
-
-
+                        int len;
+                        do
+                        {
+                            len = await stream.ReadAsync(buffer, 0, buffer.Length);
+                            sb.Append(Encoding.UTF8.GetString(buffer, 0, len));
+                        } while (len == buffer.Length);
+                        Console.WriteLine("Incoming message: {0}", sb.ToString());
+                        
                         Console.WriteLine("Sending message.");
                         byte[] message = Encoding.UTF8.GetBytes("Thank you!");
                         await stream.WriteAsync(message, 0, message.Length);
