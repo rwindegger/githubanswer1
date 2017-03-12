88af39fae46a64129122121ffbd0e2eabb27f00e
Rene Windegger <rene@windegger.wtf>
2017-03-12 15:44:38 +0100

    Implemented the state of question http://stackoverflow.com/questions/40258230/explain-weird-behavior-of-c-sharp-tcpclient-tcplistener-on-netcore

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Client
+namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartClientAsync();
+            Console.ReadLine();
+        }
+
+        public static async Task StartClientAsync()
+        {
+            using (TcpClient client = new TcpClient())
+            {
+                client.NoDelay = true;
+                client.LingerState = new LingerOption(true, 0);
+
+                Console.WriteLine("Connecting to server.");
+                await client.ConnectAsync("127.0.0.1", 1234);
+
+                Console.WriteLine("Sending message.");
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] buffer = Encoding.UTF8.GetBytes("This message is longer than what the server is willing to read.");
+                    await stream.WriteAsync(buffer, 0, buffer.Length);
+
+                    Console.WriteLine("Reading message.");
+                    int len = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    string message = Encoding.UTF8.GetString(buffer, 0, len);
+
+                    Console.WriteLine("Incoming message: {0}", message);
+                }
+            }
         }
     }
 }
--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Server
 {
@@ -6,7 +10,41 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartServerAsync();
+            Console.ReadLine();
+        }
+
+        public static async Task StartServerAsync()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 1234);
+            listener.Server.NoDelay = true;
+            listener.Server.LingerState = new LingerOption(true, 0);
+            listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for client.");
+                using (TcpClient client = await listener.AcceptTcpClientAsync())
+                {
+                    client.NoDelay = true;
+                    client.LingerState = new LingerOption(true, 0);
+
+                    Console.WriteLine("Reading message.");
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        byte[] buffer = new byte[32];
+                        int len = await stream.ReadAsync(buffer, 0, buffer.Length);
+                        string incomingMessage = Encoding.UTF8.GetString(buffer, 0, len);
+                        Console.WriteLine("Incoming message: {0}", incomingMessage);
+
+
+                        Console.WriteLine("Sending message.");
+                        byte[] message = Encoding.UTF8.GetBytes("Thank you!");
+                        await stream.WriteAsync(message, 0, message.Length);
+                        Console.WriteLine("Closing connection.");
+                    }
+                }
+            }
         }
     }
 }
